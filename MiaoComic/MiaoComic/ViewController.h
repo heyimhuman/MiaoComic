7cb64a6562425d424885b645d42337f2c733b76a
heyimhuman <kurusagi@kurusagideMacBook-Pro.local>
2016-07-10 21:56:34 +0800

    add
    创建一个工程

---
+++ MiaoComic/MiaoComic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiaoComic
+//
+//  Created by kurusagi on 16/7/10.
+//  Copyright © 2016年 kurusagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MiaoComic/MiaoComic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MiaoComic
+//
+//  Created by kurusagi on 16/7/10.
+//  Copyright © 2016年 kurusagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
